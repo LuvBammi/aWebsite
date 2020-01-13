9899a2ac2ac4b30522797a5fe758ef330d2fb9a0
fardeenmohd <fardeenmohdpl@gmail.com>
2020-01-13 21:51:13 +0100

    Add firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
-  apiKey: '#',
-  authDomain: '#',
-  databaseURL: '#',
-  projectId: '#',
-  storageBucket: '#',
-  messagingSenderId: '#',
-  appId: '#',
+  apiKey: 'AIzaSyBYrx_-Wh0jYUt7_GoIeLzUk6jhCXm6v14',
+  authDomain: 'awebsite-2153a.firebaseapp.com',
+  databaseURL: 'https://awebsite-2153a.firebaseio.com',
+  projectId: 'awebsite-2153a',
+  storageBucket: 'awebsite-2153a.appspot.com',
+  messagingSenderId: '663164207419',
+  appId: '1:663164207419:web:22eec0761a99909967371e',
+  measurementId: "G-8HNBBK9P86"
 };
